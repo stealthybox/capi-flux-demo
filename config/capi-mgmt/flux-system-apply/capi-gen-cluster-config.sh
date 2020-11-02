0527c0238b2b0b7f32ca681b98e8117d378d5280
leigh capili <leigh@null.net>
2020-11-02 14:26:15 -0700

    Add worker node to child cluster so podinfo schedules

--- config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
+++ config/capi-mgmt/flux-system-apply/capi-gen-cluster-config.sh
@@ -1,6 +1,7 @@
 clusterctl \
   --config ~/.cluster-api/dev-repository/config.yaml \
   config cluster child2 \
+  -n flux-system \
   --kubernetes-version 1.19.1 \
   -i docker:v0.3.8 \
-  -n flux-system
+  --worker-machine-count 1 \
